28ab511d22d29fe35317b664257aca6d66e6ee9c
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-03-06 12:33:11 +0700

    feat: :sparkles: basic public r3 endpoint

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export interface Env {
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+	MY_BUCKET: R2Bucket;
 	//
 	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
 	// MY_SERVICE: Fetcher;
@@ -27,6 +27,31 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const key = url.pathname.slice(1);
+
+		switch (request.method) {
+			case 'GET':
+				const object = await env.MY_BUCKET.get(key);
+
+				if (object === null) {
+					return new Response('Object Not Found', { status: 404 });
+				}
+
+				const headers = new Headers();
+				object.writeHttpMetadata(headers);
+				headers.set('etag', object.httpEtag);
+
+				return new Response(object.body, {
+					headers,
+				});
+			default:
+				return new Response('Method Not Allowed', {
+					status: 405,
+					headers: {
+						Allow: 'PUT, GET, DELETE',
+					},
+				});
+		}
 	},
 };
